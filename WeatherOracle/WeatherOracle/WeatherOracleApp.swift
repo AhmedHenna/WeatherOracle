85103f36e04beb291361bc3956d4087295448ed8
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-07-02 22:18:24 +0300

    connected to xcode

---
+++ WeatherOracle/WeatherOracle/WeatherOracleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherOracleApp.swift
+//  WeatherOracle
+//
+//  Created by Ahmed Henna on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherOracleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
