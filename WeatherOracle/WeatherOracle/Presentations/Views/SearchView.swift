9a17147e903acac5cd104bcafcc07b28b23a9a3f
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-07-12 15:13:52 +0300

    updated search view

--- WeatherOracle/WeatherOracle/Presentations/Views/SearchView.swift
+++ WeatherOracle/WeatherOracle/Presentations/Views/SearchView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State var text = ""
+    
     var body: some View {
-        WeatherCard(forecast: Forecast(date: .now, weather: .clear, probability: 0, temperature: 19, high: 28, low: 22, location: "Cairo, Egypt", icon: "Sun"))
+        NavigationView {
+            VStack{
+                weatherCards
+            }
+            .padding(.vertical, 8)
+            .background(Color.linearBackgroundMorning)
+            .searchable(text: $text, placement:
+                    .navigationBarDrawer(displayMode: .always), prompt: Text("Cairo, New York, Dubai")){
+                        ForEach(Forecast.cities){ suggestion in
+                            if text == "" {
+                                Button {
+                                    text = suggestion.location
+                                } label:{
+                                    Text(suggestion.location)
+                                        .searchCompletion(suggestion.location)
+                                }
+                            }
+                        }
+                    }
+        }
+    }
+    
+    
+    var weatherCards: some View{
+        ScrollView{
+            VStack(alignment: .leading, spacing: 20) {
+                ForEach(Forecast.cities) { city in
+                    WeatherCard(forecast: city)
+                }
+            }.frame(maxWidth: .infinity)
+        }
+        
     }
 }
+
+
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
