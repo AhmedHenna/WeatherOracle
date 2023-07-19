829936c024a74056e535d74b02d620a1f9b29bc4
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-07-19 12:31:17 +0300

    added humidity widget card

---
+++ WeatherOracle/WeatherOracle/Presentations/Views/Widget Cards/HumidityCard.swift
@@ -0,0 +1,39 @@
+//
+//  HumidityCard.swift
+//  WeatherOracle
+//
+//  Created by Ahmed Henna on 7/19/23.
+//
+
+import SwiftUI
+
+struct HumidityCard: View {
+    @State var humidity : Int
+    @State var dewPoint : Int
+    
+    
+    var body: some View {
+        WidgetCardContainer(icon: "humidity", title: "Humidity") {
+            content
+        }
+    }
+    
+    var content: some View{
+        VStack (alignment: .leading, spacing: 10){
+            Text("\(humidity)%")
+                .font(.title)
+            
+            Spacer()
+            
+            Text("The dew point is \(dewPoint)° right now")
+            .font(.caption)
+            .offset(y: -5)
+        }
+        .foregroundColor(.white)
+    }
+}
+struct HumidityCard_Previews: PreviewProvider {
+    static var previews: some View {
+        HumidityCard(humidity: 83, dewPoint: 19)
+    }
+}
