9e1c8740fe9a8a3635fb52b217d47ace26ba2646
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-07-31 17:04:45 +0300

    added a way to map API id to weather icons

---
+++ WeatherOracle/WeatherOracle/Presentations/Helpers/Mapping.swift
@@ -0,0 +1,115 @@
+//
+//  MapDescriptionToIcon.swift
+//  WeatherOracle
+//
+//  Created by Ahmed Henna on 7/31/23.
+//
+
+import Foundation
+
+func mapToIcon(id: Int, isMorning: Bool) -> String{
+    
+    if isMorning{
+        switch id{
+            //Thunderstorm
+        case 210, 211, 212, 221:
+            return "Day Cloud With Lightning"
+        case 200, 201, 202, 230, 231, 232:
+            return "Day Windy Angled Rain Drops With Lightning"
+            //Drizzle
+        case 300, 301, 310:
+            return "Rain Drops"
+        case 302, 311, 312, 313, 314, 321:
+            return "Windy Angled Rain Drops"
+            //Rain
+        case 500, 501:
+            return "Day Partly Cloudy With Rain Drops"
+        case 502, 503:
+            return "Day Partly Cloudy With Rain"
+        case 504:
+            return "Day Partly Cloudy With Angled Rain"
+        case 511:
+            return "Day Snowing Cloud"
+        case 520, 521:
+            return "Day Rain Drops"
+        case 522, 531:
+            return "Day Angled Rainy"
+            //Snow
+        case 600, 601:
+            return "Windy Snow"
+        case 602, 611, 612, 613:
+            return "Snow"
+        case 615, 616, 620, 621, 622:
+            return "Day Clouds With Snow"
+            //Atmosphere
+        case 701, 711, 721, 731, 741, 751, 761, 762, 771:
+            return "Day Windy"
+        case 781:
+            return "Tornado"
+            //Clear
+        case 800:
+            return "Sun"
+            //Clouds
+        case 801, 802:
+            return "Day Partly Cloudy"
+        case 803:
+            return "Sunny Cloud"
+        case 804:
+            return "Cloudy Sunny Clouds"
+      
+        default:
+            return "Sun"
+        }
+    }else if !isMorning{
+        switch id{
+            //Thunderstorm
+        case 210, 211, 212, 221:
+            return "Night Cloud With Lightning"
+        case 200, 201, 202, 230, 231, 232:
+            return "Night Windy Angled Rain Drops With Lightning"
+            //Drizzle
+        case 300, 301, 310:
+            return "Rain Drops"
+        case 302, 311, 312, 313, 314, 321:
+            return "Windy Angled Rain Drops"
+            //Rain
+        case 500, 501:
+            return "Night Partly Cloudy With Rain Drops"
+        case 502, 503:
+            return "Night Partly Cloudy With Rain"
+        case 504:
+            return "Night Partly Cloudy With Angled Rain"
+        case 511:
+            return "Night Snowing Cloud"
+        case 520, 521:
+            return "Night Rain Drops"
+        case 522, 531:
+            return "Night Angled Rainy"
+            //Snow
+        case 600, 601:
+            return "Windy Snow"
+        case 602, 611, 612, 613:
+            return "Snow"
+        case 615, 616, 620, 621, 622:
+            return "Night Clouds With Snow"
+            //Atmosphere
+        case 701, 711, 721, 731, 741, 751, 761, 762, 771:
+            return "Night Windy"
+        case 781:
+            return "Tornado"
+            //Clear
+        case 800:
+            return "Moon"
+            //Clouds
+        case 801, 802:
+            return "Night Partly Cloudy"
+        case 803:
+            return "Night Windy"
+        case 804:
+            return "Night Cloud"
+        default:
+            return "Moon"
+        }
+    }
+    return "Sun"
+}
